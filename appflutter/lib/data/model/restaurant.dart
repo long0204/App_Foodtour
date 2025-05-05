442c7ee872011dda51c74ba4efcb242519e58a22
long0204 <longpdh02@gmail.com>
2025-05-05 15:43:38 +0700

    commit clean code

---
+++ appflutter/lib/data/model/restaurant.dart
@@ -0,0 +1,42 @@
+import 'package:hive/hive.dart';
+
+part 'restaurant.g.dart';
+
+@HiveType(typeId: 0)
+class Restaurant extends HiveObject {
+  @HiveField(0)
+  String id;
+
+  @HiveField(1)
+  String name;
+
+  @HiveField(2)
+  String address;
+
+  @HiveField(3)
+  String type;
+
+  @HiveField(4)
+  String price;
+
+  Restaurant({required this.id, required this.name, required this.address, required this.type, required this.price});
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return Restaurant(
+      id: json['STT'].toString(),
+      name: json['Tên quán'] ?? '',
+      address: json['Địa chỉ'] ?? '',
+      type: json['Loại'] ?? '',
+      price: json['Giá'] ?? '',
+    );
+  }
+  Map<String, dynamic> toMap() {
+    return {
+      "STT": id,
+      "Loại": type,
+      "Tên quán": name,
+      "Địa chỉ": address,
+      "Giá": price,
+    };
+  }
+}
