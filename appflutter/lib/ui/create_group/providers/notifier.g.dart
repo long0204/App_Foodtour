a4d4e31c81e15d78516497d4692547c394426b34
long0204 <longpdh02@gmail.com>
2025-05-28 16:34:07 +0700

    commit notifier.g

---
+++ appflutter/lib/ui/create_group/providers/notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$groupNotifierHash() => r'3241173702643d48c7970158a3a621a8dd412896';
+
+/// See also [GroupNotifier].
+@ProviderFor(GroupNotifier)
+final groupNotifierProvider =
+    AutoDisposeNotifierProvider<GroupNotifier, GroupState>.internal(
+  GroupNotifier.new,
+  name: r'groupNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$groupNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GroupNotifier = AutoDisposeNotifier<GroupState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
